704663f9a85cd2fe64bcef7b6aa9fe5070669ca8
Maykon Franchini <maykonfranchini2017@gmail.com>
2024-01-09 23:25:05 +0000

    create db credentials + tests setup + api/v1/status tests

---
+++ tests/integration/status/api/v1/status/get.spec.ts
@@ -0,0 +1,13 @@
+test('Testing setup', async ()=> {
+  const res = await fetch('http://localhost:3000/api/v1/status')
+  const resBody = await res.json()
+  
+  const actualDate = new Date(resBody.updated_at).toISOString()
+
+  expect(res.status).toBe(200)
+  expect(resBody.updated_at).toBe(actualDate)
+  expect(resBody.dependencies.database.version).toBe('16.0')
+  expect(resBody.dependencies.database.max_connections).toBe(100)
+  expect(resBody.dependencies.database.open_connections).toBe(1)
+  
+})
