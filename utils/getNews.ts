2febc02eb20d31015a58e04851594cf73b16bea5
Maykon Franchini <maykonfranchini2017@gmail.com>
2024-01-19 03:09:13 +0000

    add source name and logo to club news fetch

--- utils/getNews.ts
+++ utils/getNews.ts
@@ -9,7 +9,7 @@ export const latestsNews = async(source_url: string) =>{
   const $ = cheerio.load(html)
     
 
-    $(".feed-post-body", html).each(function() {
+    $(".feed-post-body", html).each(function():void {
       const title = $(this).find('._evt').text()
       const link = $(this).find('._evt').find('a').attr('href')
       const img_url = $(this).find('.feed-media-wrapper').find('img').attr('src')
@@ -17,7 +17,9 @@ export const latestsNews = async(source_url: string) =>{
       news.push({
         title,
         link,
-        img_url
+        img_url,
+        source_img: 'https://s2.glbimg.com/qAiq9Ny0KA4EmGK-aiRLQ8-y3-s=/0x0:2048x2048/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/f/1/ctmjQ9SOypbLkmmJyMuw/ge.png',
+        source_name: 'globoesporte.com'
       })
     })
 
