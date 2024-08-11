c595ea8d0d9a6cc524de371dae45abfe13a1d154
MaykonFranchini <maykonfranchini2017@gmail.com>
2024-08-11 03:37:36 +0100

    api route to get news about a specific club

---
+++ pages/api/v1/clubnews/index.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { latestsNews } from "@/utils/getNews";
+import prisma from '@/infra/database'
+
+export default async function clubNews(req: NextApiRequest, res: NextApiResponse) {
+  const clubId = req.query.id
+
+  const club = await prisma.club.findFirst({
+    where: {
+      id: clubId
+    }
+  })
+  
+  const newsList = await latestsNews(club.source_url)
+  res.status(200).json({
+    newsList
+  })
+}
