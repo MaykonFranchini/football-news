12601ed3f3b48b5fabdd983272cb476e9a089f50
MaykonFranchini <maykonfranchini2017@gmail.com>
2024-08-11 03:55:32 +0100

    refactory of requests

--- pages/api/v1/clubnews/index.ts
+++ pages/api/v1/clubnews/index.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { latestsNews } from "@/utils/getNews";
-import prisma from '@/infra/database'
+import { latestsNews } from "../../../../utils/getNews";
+import prisma from '../../../../infra/database'
 
 export default async function clubNews(req, res) {
   const clubId = req.query.id
@@ -11,7 +11,7 @@ export default async function clubNews(req, res) {
     }
   })
   
-  const newsList = await latestsNews(club.source_url)
+  const newsList = await latestsNews(club!.source_url)
   res.status(200).json({
     newsList
   })
